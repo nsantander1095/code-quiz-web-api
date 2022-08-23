85a4b716bddecb0437bdf16bef4fca62decb8bd7
Nick <nsantander1095@gmail.com>
2022-08-23 09:57:44 -0400

    added comments to parts of the script file

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// starting state is just 'start'
 var state = "start";
+// grabbing the individual elements that I need
 var startEl = document.querySelector("#start");
 var quizEl = document.querySelector("#quiz");
 var endEl = document.querySelector("#end");
@@ -7,9 +9,11 @@ var submitScoreBtn = document.querySelector("#end button");
 var quizTitle = document.querySelector("#quiz #title");
 var questionsEl = document.querySelector("#questions");
 
+// will create an array of questions that are fully built out
+
 var questions = ["First", "Second", "Third", "Fourth"];
 var cursor = 0;
-
+// state manager
 function displayState() {
     if (state === "start") {
         startEl.style.display = "block";
@@ -27,32 +31,32 @@ function displayState() {
         endEl.style.display = "block";
     }
 }
-
+// initializer
 function init() {
     displayState();
 }
-
+// reset function
 function reset() {
     state = "start";
     cursor = 0;
     init();
 }
-
+// displays the questions in order
 function displayQuestion() {
     state = "quiz";
     var titleText = questions[cursor];
     quizTitle.textContent = titleText;
 }
-
+// resets the quiz when score is submitted
 submitScoreBtn.addEventListener("click", function() {
     reset();
 })
-
+// starts quiz, displays first question
 startBtn.addEventListener("click", function() {
     displayQuestion();
     displayState();
 })
-
+// continues to display questions in order
 quizTitle.addEventListener("click", function (event) {
     var element = event.target;
     if (element.matches("h2")) {
@@ -68,5 +72,5 @@ quizTitle.addEventListener("click", function (event) {
         }
     }
 })
-
+// calling initializer
 init();
