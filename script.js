6ababf4fb8566f352173756884836fb312fb9b5f
Nick <nsantander1095@gmail.com>
2022-08-24 20:26:32 -0400

    Worked with tutor to get something that functioned and selected the correct answer for the questions. I wasn't able to finish the highscores and timer in time to submit with the extension but I will most likely resubmit for a higher grade.

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ var state = "start";
 var startEl = document.querySelector("#start");
 var quizEl = document.querySelector("#quiz");
 var endEl = document.querySelector("#end");
+var highscoreEl = document.querySelector("#highscores");
 var startBtn = document.querySelector("#start button");
 var submitScoreBtn = document.querySelector("#end button");
 var quizTitle = document.querySelector("#quiz #title");
 var questionsEl = document.querySelector("#questions");
+var cursor = 0;
 
 // will create an array of questions that are fully built out
 
@@ -44,74 +46,143 @@ var questions = [
     correct: "quotes",
   },
 ];
-var cursor = 0;
 // state manager
-function displayState() {
-  if (state === "start") {
-    startEl.style.display = "block";
-    quizEl.style.display = "none";
-    endEl.style.display = "none";
-  }
-  if (state === "quiz") {
-    startEl.style.display = "none";
-    quizEl.style.display = "block";
-    endEl.style.display = "none";
-    displayQuestion();
-  }
-  if (state === "end") {
-    startEl.style.display = "none";
-    quizEl.style.display = "none";
-    endEl.style.display = "block";
-  }
-}
+// function displayState() {
+//   if (state === "start") {
+//     startEl.style.display = "block";
+//     quizEl.style.display = "none";
+//     endEl.style.display = "none";
+//     highscoreEl.style.display = "none";
+//   }
+//   if (state === "quiz") {
+//     startEl.style.display = "none";
+//     quizEl.style.display = "block";
+//     endEl.style.display = "none";
+//     highscoreEl.style.display = "none";
+//     displayQuestion();
+//   }
+//   if (state === "end") {
+//     startEl.style.display = "none";
+//     quizEl.style.display = "none";
+//     endEl.style.display = "block";
+//     highscoreEl.style.display = "none";
+//   }
+//   if (state === "highscores") {
+//     startEl.style.display = "none";
+//     quizEl.style.display = "none";
+//     endEl.style.display = "none";
+//     highscoreEl.style.display = "block";
+//   }
+// }
 // initializer
-function init() {
-  displayState();
-}
+// function init() {
+//   displayState();
+// }
 // reset function
-function reset() {
-  state = "start";
-  cursor = 0;
-  init();
-}
+// function reset() {
+//   state = "start";
+//   cursor = 0;
+//   init();
+// }
 // displays the questions in order
 function displayQuestion() {
-  state = "quiz";
+  startEl.classList.add("hide");
+  quizEl.classList.remove("hide");
   var question = questions[cursor];
   questionsEl.innerHTML = null;
   quizTitle.textContent = question.title;
   for (answer of question.possibleChoices) {
     var liEl = document.createElement("button");
+    liEl.classList.add("answer");
     liEl.textContent = answer;
     questionsEl.appendChild(liEl);
   }
-  return liEl;
-//   code below is broken, didnt want to delete all at once so I know what I was trying to do.
-//   for (var i = 0; i < questions[cursor].possibleChoices.length; i++) {
-//     var choiceButton = document.createElement("button");
-//     choiceButton.textContent = questions[cursor].possibleChoices[i];
-//     choiceButton.addEventListener("click", function () {
-//       checkChoice();
-//     });
-//     quizEl.append(choiceButton);
-//   }
+  //   code below is broken, didnt want to delete all at once so I know what I was trying to do.
+  //   for (var i = 0; i < questions[cursor].possibleChoices.length; i++) {
+  //     var choiceButton = document.createElement("button");
+  //     choiceButton.textContent = questions[cursor].possibleChoices[i];
+  //     choiceButton.addEventListener("click", function () {
+  //       checkChoice();
+  //     });
+  //     quizEl.append(choiceButton);
+  //   }
+  addListeners();
 }
 
-function checkChoice(choice) {
-  if (choice == questions[cursor].correct.value) {
+function checkAnswer(event) {
+  console.log(event.target.innerHTML);
+  if (event.target.innerHTML === questions[cursor].correct) {
     alert("Correct");
   } else {
     alert("Wrong");
   }
+  cursor++;
+  checkQuizOver();
 }
+
+function addListeners() {
+  var choices = document.getElementsByClassName("answer");
+  console.log(choices);
+  for (let i = 0; i < choices.length; i++) {
+    choices[i].addEventListener("click", checkAnswer);
+  }
+}
+//    choice.addEventListener("click", function(e){
+//     e.preventDefault()
+//     console.log(cursor)
+//     console.log(questions[cursor])
+//     console.log(option)
+//     if (option=== questions[cursor].correct) {
+//             alert("Correct");
+//           } else {
+//             alert("Wrong");
+//           }
+//           cursor++
+//           checkQuizOver()
+//    })
+
+function checkQuizOver() {
+  if (cursor === questions.length) {
+    return endQuiz();
+  }
+  displayQuestion();
+}
+
+function endQuiz() {
+  quizEl.classList.add("hide");
+  endEl.classList.remove("hide");
+  var submitInitials = document.getElementById("initials");
+  console.log(submitInitials);
+}
+
+function displayHighscore() {
+  state = "highscores";
+  // local storage stuff
+  var resetEl = document.createElement("button");
+  resetEl.textContent = "Reset Quiz";
+  highscoreEl.appendChild(resetEl);
+}
+
+// function checkChoice() {
+//     e.preventDefault()
+//   if (choice == questions[cursor].correct) {
+//     alert("Correct");
+//   } else {
+//     alert("Wrong");
+//   }
+//   checkQuizOver()
+// }
 // resets the quiz when score is submitted
 submitScoreBtn.addEventListener("click", function () {
+  displayHighscore();
   reset();
 });
 // starts quiz, displays first question
-startBtn.addEventListener("click", function () {
+startBtn.addEventListener("click", function (e) {
+  cursor = 0;
+  e.preventDefault();
   displayQuestion();
-  displayState();
+  //displayState();
 });
 
 // liEl.addEventListener("click", function(event) {
@@ -148,4 +219,4 @@ quizTitle.addEventListener("click", function (event) {
   }
 });
 // calling initializer
-init();
+//init();
